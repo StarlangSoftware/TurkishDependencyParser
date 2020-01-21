0bc06931769311cbd493d01ff78901759a33214a
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2020-01-21 10:50:41 +0300

    Added NSUBJPASS and CSUBJPASS dependencies.

--- src/main/java/DependencyParser/UniversalDependencyRelation.java
+++ src/main/java/DependencyParser/UniversalDependencyRelation.java
@@ -5,16 +5,16 @@ public class UniversalDependencyRelation extends DependencyRelation{
     private UniversalDependencyType universalDependencyType;
 
     public static final String[] universalDependencyTypes = {"acl", "advcl", "advmod", "amod", "appos", "aux",
-            "case", "cc", "ccomp", "clf", "compound", "conj", "cop", "csubj", "dep", "det", "discourse", "dislocated",
-            "expl", "fixed", "flat", "goeswith", "iobj", "list", "mark", "nmod", "neg", "nsubj", "nummod", "obj", "obl", "orphan",
+            "case", "cc", "ccomp", "clf", "compound", "conj", "cop", "csubj", "csubjpass", "dep", "det", "discourse", "dislocated",
+            "expl", "fixed", "flat", "goeswith", "iobj", "list", "mark", "nmod", "neg", "nsubj", "nsubjpass", "nummod", "obj", "obl", "orphan",
             "parataxis", "punct", "reparandum", "root", "vocative", "xcomp"};
     public static final UniversalDependencyType[] universalDependencyTags = {UniversalDependencyType.ACL, UniversalDependencyType.ADVCL,
             UniversalDependencyType.ADVMOD, UniversalDependencyType.AMOD, UniversalDependencyType.APPOS, UniversalDependencyType.AUX, UniversalDependencyType.CASE,
             UniversalDependencyType.CC, UniversalDependencyType.CCOMP, UniversalDependencyType.CLF, UniversalDependencyType.COMPOUND, UniversalDependencyType.CONJ,
-            UniversalDependencyType.COP, UniversalDependencyType.CSUBJ, UniversalDependencyType.DEP, UniversalDependencyType.DET, UniversalDependencyType.DISCOURSE,
+            UniversalDependencyType.COP, UniversalDependencyType.CSUBJ, UniversalDependencyType.CSUBJPASS, UniversalDependencyType.DEP, UniversalDependencyType.DET, UniversalDependencyType.DISCOURSE,
             UniversalDependencyType.DISLOCATED, UniversalDependencyType.EXPL, UniversalDependencyType.FIXED, UniversalDependencyType.FLAT,
             UniversalDependencyType.GOESWITH, UniversalDependencyType.IOBJ, UniversalDependencyType.LIST, UniversalDependencyType.MARK, UniversalDependencyType.NMOD,
-            UniversalDependencyType.NEG, UniversalDependencyType.NSUBJ, UniversalDependencyType.NUMMOD, UniversalDependencyType.OBJ, UniversalDependencyType.OBL, UniversalDependencyType.ORPHAN,
+            UniversalDependencyType.NEG, UniversalDependencyType.NSUBJ, UniversalDependencyType.NSUBJPASS, UniversalDependencyType.NUMMOD, UniversalDependencyType.OBJ, UniversalDependencyType.OBL, UniversalDependencyType.ORPHAN,
             UniversalDependencyType.PARATAXIS, UniversalDependencyType.PUNCT, UniversalDependencyType.REPARANDUM, UniversalDependencyType.ROOT,
             UniversalDependencyType.VOCATIVE, UniversalDependencyType.XCOMP};
 
