088c4671cd738bd43151c63de0e3dee7af310ef2
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-22 15:10:08 +0300

    Added datasets annotated by Starlang and modified the constructor of UniversalDependencyTreeBankCorpus and UniversalDependencyTreeBankSentence

--- src/main/java/DependencyParser/Universal/UniversalDependencyTreeBankCorpus.java
+++ src/main/java/DependencyParser/Universal/UniversalDependencyTreeBankCorpus.java
@@ -16,51 +16,16 @@ public class UniversalDependencyTreeBankCorpus extends Corpus{
 
     public UniversalDependencyTreeBankCorpus(String fileName){
         try {
-            UniversalDependencyTreeBankSentence sentence = null;
-            UniversalDependencyRelation relation;
             ClassLoader classLoader = getClass().getClassLoader();
             BufferedReader br = new BufferedReader(new InputStreamReader(classLoader.getResourceAsStream(fileName), StandardCharsets.UTF_8));
+            String sentence = "";
             String line = br.readLine();
             while (line != null) {
                 if (line.length() == 0){
-                    addSentence(sentence);
-                    sentence = null;
+                    addSentence(new UniversalDependencyTreeBankSentence(sentence));
+                    sentence = "";
                 } else {
-                    if (line.startsWith("#")){
-                        if (sentence == null){
-                            sentence = new UniversalDependencyTreeBankSentence();
-                        }
-                        sentence.addComment(line.trim());
-                    } else {
-                        String[] items = line.split("\\t");
-                        if (items.length != 10){
-                            System.out.println("Line does not contain 10 items ->" + line);
-                        } else {
-                            String id = items[0];
-                            if (id.matches("\\d+")){
-                                String surfaceForm = items[1];
-                                String lemma = items[2];
-                                UniversalDependencyPosType upos = UniversalDependencyRelation.getDependencyPosType(items[3]);
-                                if (upos == null){
-                                    System.out.println("Line does not contain universal pos ->" + line);
-                                }
-                                String xpos = items[4];
-                                UniversalDependencyTreeBankFeatures features = new UniversalDependencyTreeBankFeatures(items[5]);
-                                if (!items[6].equals("_")){
-                                    int to = Integer.parseInt(items[6]);
-                                    String dependencyType = items[7].toUpperCase();
-                                    relation = new UniversalDependencyRelation(to, dependencyType);
-                                } else {
-                                    relation = null;
-                                }
-                                String deps = items[8];
-                                String misc = items[9];
-                                UniversalDependencyTreeBankWord word = new UniversalDependencyTreeBankWord(Integer.parseInt(id), surfaceForm,
-                                        lemma, upos, xpos, features, relation, deps, misc);
-                                sentence.addWord(word);
-                            }
-                        }
-                    }
+                    sentence += line + "\n";
                 }
                 line = br.readLine();
             }
--- src/main/java/DependencyParser/Universal/UniversalDependencyTreeBankSentence.java
+++ src/main/java/DependencyParser/Universal/UniversalDependencyTreeBankSentence.java
@@ -14,6 +14,46 @@ public class UniversalDependencyTreeBankSentence extends Sentence {
         comments = new ArrayList<>();
     }
 
+    public UniversalDependencyTreeBankSentence(String sentence){
+        this();
+        UniversalDependencyRelation relation;
+        String[] lines = sentence.split("\\n");
+        for (String line : lines){
+            if (line.startsWith("#")){
+                addComment(line.trim());
+            } else {
+                String[] items = line.split("\\t");
+                if (items.length != 10){
+                    System.out.println("Line does not contain 10 items ->" + line);
+                } else {
+                    String id = items[0];
+                    if (id.matches("\\d+")){
+                        String surfaceForm = items[1];
+                        String lemma = items[2];
+                        UniversalDependencyPosType upos = UniversalDependencyRelation.getDependencyPosType(items[3]);
+                        if (upos == null){
+                            System.out.println("Line does not contain universal pos ->" + line);
+                        }
+                        String xpos = items[4];
+                        UniversalDependencyTreeBankFeatures features = new UniversalDependencyTreeBankFeatures(items[5]);
+                        if (!items[6].equals("_")){
+                            int to = Integer.parseInt(items[6]);
+                            String dependencyType = items[7].toUpperCase();
+                            relation = new UniversalDependencyRelation(to, dependencyType);
+                        } else {
+                            relation = null;
+                        }
+                        String deps = items[8];
+                        String misc = items[9];
+                        UniversalDependencyTreeBankWord word = new UniversalDependencyTreeBankWord(Integer.parseInt(id), surfaceForm,
+                                lemma, upos, xpos, features, relation, deps, misc);
+                        addWord(word);
+                    }
+                }
+            }
+        }
+    }
+
     public void addComment(String comment){
         comments.add(comment);
     }
